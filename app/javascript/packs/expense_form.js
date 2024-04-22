fefa388183342338bac6249e00c8653af262ca08
prateek0411999 <prateekshrmprateek@gmail.com>
2024-04-22 22:47:05 +0530

    added support for non_itemized expense

--- app/javascript/packs/expense_form.js
+++ app/javascript/packs/expense_form.js
@@ -9,8 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const expense_sharers =  document.getElementById("expense_sharer_ids");
     const totalAmountInput = document.getElementById('expense_total_amount');
     const expense_form = document.getElementById("expense-form");
+    const itemContainer = $("#item-container");
+    
+    let itemCount = 0; // Counter for item inputs
     
     // Handlers
+    expense_sharers.addEventListener('change', function() {
+        // Enable total amount input only if at least one sharer is selected
+        if(expense_types.selectedOptions[0].value == "itemized"){
+            updateSelectOptions() 
+        }else{
+            totalAmountInput.disabled = expense_sharers.value.length === 0;
+
+        }
+    });
+
     split_types.addEventListener('change', function() {
         if (this.value === 'equally') {
             share_by_percentage_div.innerHTML = "";
@@ -25,10 +38,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
     
-    expense_sharers.addEventListener('change', function() {
-        // Enable total amount input only if at least one sharer is selected
-        totalAmountInput.disabled = expense_sharers.value.length === 0;
-    });
 
     expense_types.addEventListener('change', function() {
         if(expense_types.selectedOptions[0].value == "non_itemized")
@@ -43,12 +52,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     $('#expense_sharer_ids').on("change", function() {
+        updateSharersDropdown();
+        if(expense_types.selectedOptions[0].value == "non_itemized"){
            if(expense_sharers.selectedOptions.length == 0)
            {
             totalAmountInput.disabled = true;
             split_types.value = "equally";
             split_types.dispatchEvent(new Event('change'));
-           }else{
+           }
+           else{
             totalAmountInput.removeAttribute("disabled")
             // selected Option
             let selected_split_type = split_types.selectedOptions[0].value;
@@ -57,9 +69,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 split_types.dispatchEvent(new Event('change'));
             }
            }
+        }else {
+            document.getElementById("item-container").innerHTML= "";
+            itemCount = 0;
+            $("#total-value-container").hide()
         }
-    )
+    })
 
+    $("#expense_payer_id").on("change", function() {
+        console.log('@!@@@@@@')
+        updateSharersDropdown();
+    })
+ 
     function fetchExpenseSharers()
     {
         let selectedValues = [];
@@ -132,11 +153,39 @@ document.addEventListener('DOMContentLoaded', function() {
         return errors;
     }
 
+    function updateSharersDropdown() {
+        const selectedPayerId = $("#expense_payer_id").val();
+        const selectedSharerIds = $("#expense_sharer_ids").val();
+        
+
+        console.log("selected payer", selectedPayerId)
+        console.log("selected sharer ids ", selectedPayerId)
+        // Remove the selected payer from the sharers dropdown
+        $("#expense_sharer_ids").selectize();
+        $("#expense_sharer_ids option").each(function() {
+            const optionValue = $(this).val();
+            if (optionValue === selectedPayerId) {
+                $(this).prop("disabled", true).hide();
+            } else {
+                $(this).prop("disabled", false).show();
+            }
+        });
+        
+        // Remove the selected sharers from the payer dropdown
+        $("#expense_payer_id option").each(function() {
+            const optionValue = $(this).val();
+            if (selectedSharerIds.includes(optionValue)) {
+                $(this).prop("disabled", true).hide();
+            } else {
+                $(this).prop("disabled", false).show();
+            }
+        });
+    }
+
     
 
     expense_form.addEventListener("submit", function(e) {
-       e.preventDefault();
-
+        e.preventDefault();
         // Perform custom validation
         let is_form_valid = validateForm()
         if (is_form_valid.valid) {
@@ -179,5 +228,172 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
+
+
+
+
+
+
+
+    function updateSelectOptions() {
+        const selectedSharers = $("#expense_sharer_ids").html(); // Get updated options
+        $(".sharer-select").html(selectedSharers); // Update options in all select tags
+    }
+
+    // Add event listener to add item button
+    $("#add-item").click(function() {
+    
+        if ($("#expense_sharer_ids").val().length == 0) {
+        // Display SweetAlert if no sharer is selected
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please select at least one sharer before adding an item!',
+        });
+        return; // Exit function if no sharer is selected
+        }
+        itemCount++; // Increment counter
+        if(itemCount > 0)
+        {
+            document.getElementById("total-value-container").style.display = "block"
+        }
+        const htmlString = `
+        <div class="card mb-3" style="margin-bottom: 15px;">
+            <div class="card-body">
+            <div class="row mb-3">
+                <div class="col-md-4">
+                <label for="item-description-label-${itemCount}" class="form-label">Item Description</label>
+                <input type="text" class="form-control" id="item-description-input-${itemCount}" name="expense[custom_sharer_expenses][][${itemCount}][description]" placeholder="Enter item description" required>
+                </div>
+                <div class="col-md-3">
+                <label for="item-amount-label-${itemCount}" class="form-label">Item Amount</label>
+                <input type="number" class="form-control item-amount" id="item-amount-input-${itemCount}" name="expense[custom_sharer_expenses][][${itemCount}][amount]" placeholder="Enter item amount" step="0.01" required>
+                </div>
+                <div class="col-md-4">
+                <div class="form-check ">
+                    <div class="custom-sharer-columns">
+                ${$("#expense_sharer_ids").find(":selected").map(function(index, element) {
+                return `
+                    <div><label for="sharer-share-${itemCount}-${element.value}" class="form-label" style="display: block;">${element.text}</label></div>
+                    <input class="form-check-input expense-sharers-input expense-sharers-${itemCount}" type="text" id="sharer-share-${itemCount}-${element.value}" readonly>
+                `;
+                }).get().join("")}
+
+                ${`
+                    <div><label for="sharer-share-${itemCount}-${payer.selectedOptions[0].value}" class="form-label" style="display: block;">${payer.selectedOptions[0].text}</label></div>
+                    <input class="form-check-input expense-sharers-input  expense-sharers-${itemCount}" type="text" id="sharer-share-${itemCount}-${payer.selectedOptions[0].value}" readonly>`
+                }
+                    </div>
+                    <label for="item-split by ${itemCount}" class="form-label" style="display: block;">Split Item</label>
+                    <input class="form-check-input split-checkbox" name="expense[custom_sharer_expenses][][${itemCount}][split_equally]" type="checkbox" id="split-equally-${itemCount}" checked>
+                    <label class="form-check-label" for="split-equally-${itemCount}">Split Equally</label>
+                    <select class="form-select sharer-select" name="expense[custom_sharer_expenses][][${itemCount}][sharer]" style="display: none;"></select>
+                </div>  
+                </div>
+                <div class="col-md-1">
+                <button type="button" class="btn btn-danger remove-item">Remove Item</button>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-md-12 ">
+                <!-- Dynamic columns for sharers will be added here if splitting equally else just one amount for the selected sharer-->
+                </div>
+            </div>
+            </div>
+        </div>
+        `;
+        itemContainer.append(htmlString);
+    });
+
+    // Add event listener for removing item
+    itemContainer.on("click", ".remove-item", function() {
+        $(this).closest(".card").remove();
+        itemCount--; // Decrement counter
+
+        if(itemCount == 0)
+        {
+            $("#total-value-container").hide()
+        }
+    });
+
+  // Add event listener to split checkbox
+    itemContainer.on("change", ".split-checkbox", function() {
+        const cardBody = $(this).closest(".card-body");
+        const customsharerColumns = cardBody.find(".custom-sharer-columns");
+        const payer = $("#expense_payer_id").find(":selected");
+        const sharerSelect = cardBody.find(".sharer-select");
+        if ($(this).is(":checked")) {
+            sharerSelect.hide(); // Hide select tag
+            customsharerColumns.append($("#expense_sharer_ids").find(":selected").map(function(index, element) {
+                    return `
+                        <div><label for="sharer-share-${itemCount}-${element.value}" class="form-label" style="display: block;">${element.text}</label></div>
+                        <input class="form-check-input expense-sharers-input expense-sharers-${itemCount}" type="text" id="sharer-share-${itemCount}-${element.value}" readonly>
+                    `;
+                    }).get().join(""))
+            
+            customsharerColumns.append(`
+                <div><label for="sharer-share-${itemCount}-${payer.val()}" class="form-label" style="display: block;">${payer.text()}</label></div>
+                <input class="form-check-input expense-sharers-input expense-sharers-${itemCount}" type="text" id="sharer-share-${itemCount}-${payer.val()}" readonly>
+            `)
+            $(".item-amount").change();
+        } else {
+            customsharerColumns.empty();
+            let selectedSharers = $("#expense_sharer_ids").html(); // Get selected sharers
+            let option = `<option value=${payer.val()}> ${payer.text()} </option>`
+            sharerSelect.html(selectedSharers); // Add options to select tag and show it
+            sharerSelect.append(option)
+            sharerSelect.show()
+        }
+    });
+
+    itemContainer.on("change", ".item-amount", function() {
+        const cardBody = $(this).closest(".card-body");
+        const isSplitEqually = cardBody.find(".split-checkbox").is(":checked");
+        const amount = parseFloat($(this).val()); // Get the new item amount
+        const sharerInputs = cardBody.find(".expense-sharers-input");
+
+        if (isSplitEqually) {
+            // If split equally, update the value for each sharer input   
+            let equalShare = amount / sharerInputs.length;
+            let equalShareValue = equalShare.toFixed(2)
+            
+            if(isNaN(equalShareValue))
+            {
+                equalShareValue = ""
+            }
+            sharerInputs.each(function() {
+                $(this).val(equalShareValue); // Update each sharer input with the equal share
+            });
+        }
+
+
+        //update grand total 
+        updateGrandTotal();
+    })
+
+
+
+    // update amount 
+    function updateGrandTotal() {
+        let subTotal = 0;
+        // Iterate over each item
+        $(".item-amount").each(function() {
+            const amount = parseFloat($(this).val());
+            subTotal += amount;
+           
+        });
+        // Update the UI with the grand total
+        let grandTotal = subTotal;
+        subTotal = subTotal.toFixed(2);
+        
+        if(isNaN(subTotal))
+        {
+            grandTotalFloat = ""
+        }else{
+            grandTotalFloat = parseFloat(subTotal) + parseFloat((subTotal * 0.245))
+        }
+        $("#sub_total").val(subTotal);
+        $("#grand_total").val(grandTotalFloat.toFixed(2));
+    }
     
 })
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -21,16 +21,12 @@ class Expense < ApplicationRecord
   after_create :create_non_itemized_sharers, if: -> { non_itemized? }
   after_create :create_itemized_sharers, if: -> { itemized? }
 
-  def create_non_itemized_sharers
-    case split_type
-    when 'equally'
-      create_equally_split_sharers
-    when 'unequally'
-      create_unequally_split_sharers
-    end
+  def sharer_ids=(ids)
+    ids.reject!(&:empty?) if ids.is_a?(Array)
+    @sharer_ids = ids
   end
 
-  def create_itemized_sharers
+  def create_non_itemized_sharers
     case split_type
     when 'equally'
       create_equally_split_sharers
@@ -41,18 +37,61 @@ class Expense < ApplicationRecord
 
   private
 
+  def amount_including_tax(amount)
+    (amount + (amount * 0.245))
+  end
+
+  def create_sharer(user_id, amount)
+    expense_sharers.create(user_id: user_id, amount: amount)
+  end
+
+  def create_item(description, amount, split_equally = nil, sharer = nil)
+    equally_splitted = (split_equally.present? &&  split_equally== "on") ?  true : false
+    items.create(description: description, amount: amount, split_equally: equally_splitted, user_id: sharer)
+  end 
+
   def create_equally_split_sharers
     total_sharers = sharer_ids.count
+    total_sharers = total_sharers + 1 #including the payer as splitting equally
     amount_per_sharer = total_amount / total_sharers
     sharer_ids.each do |user_id|
-      self.expense_sharers.create(user_id: user_id, amount: amount_per_sharer&.round(2))
+      create_sharer(user_id, amount_per_sharer&.round(2))
     end
   end
 
   def create_unequally_split_sharers
     custom_sharer_expenses.each do |user_id, prct|
-      amount = total_amount * (prct["percentage"].to_f / 100.to_f)
-      expense_sharers.create(user_id: user_id, amount: amount&.round(2))
+      amount_per_sharer = total_amount * (prct["percentage"].to_f / 100.to_f)
+      create_sharer(user_id, amount_per_sharer&.round(2))
     end
+  end
+
+  def create_itemized_sharers
+    items = custom_sharer_expenses
+    total_amount_sum = 0
+    items.each do |item_hash|
+      item_hash.each do |_, item_data|
+        description = item_data["description"]
+        amount = item_data['amount'].to_f
+        split_equally = item_data["split_equally"]
+        sharer = item_data["sharer"]&.to_i
+        total_amount_sum += amount #updating the total_amount
+        if split_equally.present? && split_equally == "on"
+          # for now taxes are pre-defined so directly inputting the value
+          total_sharers = sharer_ids.size + 1 #including the payerer
+          amount_per_user_including_taxes = ((amount_including_tax(amount)) / total_sharers)
+          sharer_ids.each do |user_id|
+            create_sharer(user_id, amount_per_user_including_taxes)
+          end
+        else
+          amount_with_tax_for_user = amount_including_tax(amount)
+          create_sharer(sharer, amount_with_tax_for_user)
+        end
+        create_item(description, amount, split_equally, sharer)
+      end
+    end
+    
+    total_amount_sum_with_tax = amount_including_tax(total_amount_sum)
+    self.update(total_amount: total_amount_sum_with_tax)
   end
 end
